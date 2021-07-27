eb6d130595fae3f6ba17903d0bd703132fa4ad59
Arecio Canton <cuba289@gmail.com>
2021-07-27 06:29:06 -0400

    added text editor header

--- components/Header.js
+++ components/Header.js
@@ -13,12 +13,12 @@ function Header() {
                 rounded={true}
                 iconOnly={true}
                 ripple="dark"
-                className="h-20 w-20 border-0"
+                className="hidden md:inline-flex h-20 w-20 border-0"
             >
                 <Icon name="menu" size="3xl" color="gray" />
             </Button>
             <Icon name="description" size="5xl" color="orange" />
-            <h1 className="ml-2 text-gray font-extralight text-2xl italic">Documaker</h1>
+            <h1 className="hidden md:inline-flex ml-2 text-gray font-extralight text-2xl italic">Documaker</h1>
 
             {/* Focus-within means when child gets focus on then apply these styles */}
             <div className="flex flex-grow items-center px-5
@@ -34,7 +34,7 @@ function Header() {
             rounded={true}
             iconOnly={true}
             ripple="dark"
-            className="ml-5 md:ml-20 h-20 w-30 border-0"
+            className="hidden md:inline-flex ml-5 md:ml-20 h-20 w-30 border-0"
             >
             <Icon name="apps" size="3xl" color="gray"/>
             </Button>
--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -11,15 +11,58 @@ import Loading from "../../components/Loading";
 function Doc() {
     const [session, loading] = useSession();
 
-    if (loading) return <Loading />;
     if (!session) return <Login />;
+    if (loading) return <Loading />;
+
+    const router = useRouter();
+    // Since we made the wild card with the name id it becpmes a query param
+    // router.query.id
+    const { id } = router.query;
+    const [snapshot, loadingSnapshot] = useDocumentOnce(db.collection('userDocs')
+    .doc(session.user.email)
+    .collection("docs")
+    .doc(id)
+    );
+
+    // if loading data is done and you dont have the filename in your db user gets redirected to home screen
+    if (!loadingSnapshot && !snapshot?.data()?.fileName) {
+        router.replace("/");
+    }
 
     return (
         <div>
             <header className="flex justify-between items-center p-3 pb-1">
-                <span onClick={() => router.push('/')}>
+                <span onClick={() => router.push('/')} className="cursor-pointer">
                 <Icon name="description" size="3xl" color="orange"/>
                 </span>
+
+                <div className="flex-grow px-2">
+                    <h2>{snapshot?.data()?.fileName}</h2>
+                    <div className="flex items-center text-sm space-x-1 -ml-1 h-8 text-gray-600">
+                        <p className="options">File</p>
+                        <p className="options">Edit</p>
+                        <p className="options">View</p>
+                        <p className="options">Insert</p>
+                        <p className="options">Format</p>
+                        <p className="options">Tools</p>
+                    </div>
+                </div>
+
+                <Button 
+                color="orange"
+                buttonType="filled"
+                size="regular"
+                className="hidden md:!inline-flex h-10"
+                rounded={false}
+                block={false}
+                iconOnly={false}
+                ripple="light"
+                >
+                   <Icon name="people" size="md"/> 
+                   Share
+                </Button>
+
+                <img src={session.user.image} className="rounded-full cursor-pointer h-10 w-10 ml-2" alt="" />
             </header>
         </div>
     )
