5caedb97dd0002cf8e3e63b359328da44950d33b
Arecio Canton <cuba289@gmail.com>
2021-07-27 05:16:32 -0400

    added doc row component

--- pages/index.js
+++ pages/index.js
@@ -10,15 +10,24 @@ import ModalFooter from "@material-tailwind/react/ModalFooter";
 import { useState } from 'react';
 import { db } from "../backend/firebase";
 import firebase from "firebase";
+import { useCollectionOnce } from "react-firebase-hooks/firestore";
+import DocumentRow from "../components/DocumentRow";
 
 export default function Home() {
   // This hook will grab the client info
   const [session, loading] = useSession();
+  if (!session) return <Login />;
+  
   const [showModal, setShowModal] = useState(false);
   const [input, setInput] = useState("");
-
-  if (!session) return <Login />;
-
+  // Real time db listener so we can order the docs by date created
+  const [snapshot] = useCollectionOnce(db.collection('userDocs')
+  .doc(session.user.email)
+  .collection('docs')
+  .orderBy('timestamp', 'desc')
+  );
+  
+  
   const createDocument = () => {
     if (!input) return;
     // makes userDocs collection
@@ -80,7 +89,7 @@ export default function Home() {
 
       <Header />
       {modal}
-      <section className="bg-[#F8F9FA] pb-10 px-10">
+      <section className="bg-[#F8F9FA] pb-10 md:px-10">
         <div className="max-w-3xl mx-auto">
           <div className="flex items-center justify-between py-6">
             <h2 className="text-gray-700 text-lg">Start a new document</h2>
@@ -97,8 +106,8 @@ export default function Home() {
           </div>
           <div>
             {/* Convert img to WP Format also optimizes for screen size */}
-            <div onClick={() => setShowModal(true)} className="relative h-50 w-40 border-2 cursor-pointer hover:border-red-300">
-              <img src="https://media.istockphoto.com/vectors/cross-plus-medical-icon-design-template-elements-vector-id487552824?k=6&m=487552824&s=612x612&w=0&h=M2LHlaVwe9ZMz8883eUjEt3keKlUibsFKfPLUFFqeEI=" loading="lazy" alt="" />
+            <div onClick={() => setShowModal(true)} className="relative h-52 w-40 border-2 cursor-pointer hover:border-red-300">
+              <img src="https://media.istockphoto.com/vectors/cross-plus-medical-icon-design-template-elements-vector-id487552824?k=6&m=487552824&s=612x612&w=0&h=M2LHlaVwe9ZMz8883eUjEt3keKlUibsFKfPLUFFqeEI=" loading="lazy" className="min-h-full min-w-full" alt="" />
             </div>
 
             <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">Blank</p>
@@ -109,11 +118,20 @@ export default function Home() {
       <section className="bg-white px-10 md:px-0">
         <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
           <div className="flex items-center justify-between pb-5">
-            <h2 className="font-medium flex-grow"><span className="color-gray-500">{session?.user?.name}'s</span> Documents</h2>
+            <h2 className="font-light text-2xl flex-grow"><span className="color-gray-500">{session?.user?.name}'s</span> Documents</h2>
             <p className="mr-12">Date Created</p>
             <Icon name="folder" size="3xl" color="orange" />
           </div>
-        </div>
+
+        {snapshot?.docs?.map((doc) => (
+          <DocumentRow 
+          key={doc.id}
+          id={doc.id}
+          fileName={doc.data().fileName}
+          date={doc.data().timestamp}
+          />
+          ))}
+          </div>
       </section>
 
     </div>
